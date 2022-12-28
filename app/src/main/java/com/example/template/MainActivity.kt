e8bb4733e209a351aa375e0b20374802cbaee3ba
William <william.porto@nave.rs>
2022-12-28 13:14:16 -0300

    adding jetpack compose

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.example.template
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
 
-class MainActivity : AppCompatActivity()
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = stringResource(R.string.app_name))
+        }
+    }
+}
