982e9177cccae97bbefe5f6b92753998b0ee4f7f
William <william.porto@nave.rs>
2022-12-28 14:02:15 -0300

    setting up default theme

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.template
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.res.stringResource
+import com.example.template.theming.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = stringResource(R.string.app_name))
+            ApplicationTheme({ this }) {
+                Scaffold {
+                    Text(text = stringResource(R.string.app_name))
+                }
+            }
         }
     }
 }
