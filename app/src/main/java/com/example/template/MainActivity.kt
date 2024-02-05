1e14a4ea44f32923190089239f9fdeadf1a7c7ba
William Porto <powilliam19@gmail.com>
2024-02-04 22:09:08 -0300

    using enableEdgeToEdge

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.template
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.core.view.WindowCompat
 import com.example.template.navigation.ApplicationNavHost
 import com.example.template.theming.ApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge();
         setContent {
             val windowSizeClass = calculateWindowSizeClass(this)
 
