d0c21482ee5d7deabb888b6edca3587d174d7010
William Porto <powilliam19@gmail.com>
2024-02-18 11:09:29 -0300

    @RunWith(AndroidJUnit4::class)

--- app/src/androidTest/java/com/example/template/InstructionsScreenTest.kt
+++ app/src/androidTest/java/com/example/template/InstructionsScreenTest.kt
@@ -7,11 +7,14 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.template.instructions.InstructionsScreen
 import com.example.template.theming.ApplicationTheme
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class InstructionsScreenTest {
     @get:Rule
     val rule = createAndroidComposeRule<ComponentActivity>()
